9a993a4e4f9c663c63e2269eb4b7593579236e78
Bindie <bindiethebot@gmail.com>
2020-09-06 14:14:24 +0200

    chore: add logging

--- src/listener.ts
+++ src/listener.ts
@@ -2,11 +2,15 @@ import { EventEmitter } from "@arkecosystem/core-interfaces";
 
 import { Events, Options, BlockApplied } from "./types";
 import TbwService from "./services/TbwService";
+import LoggerService from "./services/LoggerService";
 
 class Listener {
   setUp(options: Partial<Options>, emitter: EventEmitter.EventEmitter) {
     // Setup powerUp trigger and queue
     emitter.on(Events.BlockApplied, async ({ block }: BlockApplied) => {
+      LoggerService.getLogger().info(`vkey ========= ${options.validator.publicKey} ===========`);
+      LoggerService.getLogger().info(`block key ========= ${block.generatorPublicKey} ===========`);
+
       if (
         block.height > options.startHeight &&
         options.validator.publicKey === block.generatorPublicKey
