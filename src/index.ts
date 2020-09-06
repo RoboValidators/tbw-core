9437233b006faa091a393b2bd337a5b5fb86f7db
Bindie <bindiethebot@gmail.com>
2020-09-06 14:12:51 +0200

    chore: added app name to fb initialization

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,15 @@ import * as fireorm from "fireorm";
 import admin from "firebase-admin";
 
 import serviceAccount from "../serviceAccountKey.json";
+import { alias } from "./defaults";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
-});
+admin.initializeApp(
+  {
+    credential: admin.credential.cert(serviceAccount as any),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+  },
+  alias
+);
 
 fireorm.initialize(admin.firestore());
 
