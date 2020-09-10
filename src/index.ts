0975e694f5a9688175cf8654ccabe0d0bd505ebe
Bindie <bindiethebot@gmail.com>
2020-09-10 18:16:07 +0200

    chore: fix any case

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import "firebase/firestore";
 import * as fireorm from "fireorm";
-import admin from "firebase-admin";
+import admin, { ServiceAccount } from "firebase-admin";
 
 import serviceAccount from "../serviceAccountKey.json";
 import { alias } from "./defaults";
 
 const tbwFirebase = admin.initializeApp(
   {
-    credential: admin.credential.cert(serviceAccount as any),
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
     databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
   },
   alias
