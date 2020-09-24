14b3a47bea022b804eab501234048bc699a5ec25
Bindie <bindiethebot@gmail.com>
2020-09-24 10:38:58 +0200

    chore: log when trying to persist multiple rows for same block

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,12 +5,18 @@ import TrueBlockWeight from "./models/TrueBlockWeight";
 import VoterModel from "./models/Voters";
 import tbwRepository from "./repositories/TBWRepository";
 import voterRepository from "./repositories/VoterRepository";
+import LoggerService from "../services/plugin/LoggerService";
 
 export default class DB {
   private constructor() {}
 
   static async addTbw(tbw: TrueBlockWeight): Promise<void> {
-    await tbwRepository.create(tbw);
+    const foundTbw = await tbwRepository.findById(tbw.id);
+    if (foundTbw) {
+      LoggerService.getLogger().error(`TBW for blockheight ${tbw.id} already exists`);
+    } else {
+      await tbwRepository.create(tbw);
+    }
   }
 
   static async updatePending(voters: Voter[]): Promise<void> {
